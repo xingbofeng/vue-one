85cf7504ea2cc18e3b01de77c68bb8dd423927b8
xingbofeng <503908971@qq.com>
2017-05-06 19:27:19 +0800

    fix: async bug

--- src/router/Home/index.js
+++ src/router/Home/index.js
@@ -28,63 +28,59 @@ export default {
         store.commit(types.NET_STATUS, error);
       });
       // 请求首页所需要的一周语文、文章、问答、音乐和电影
-      oneList.content_list.forEach((value) => {
+      const oneListContent = oneList.content_list.map((value) => {
         switch (value.content_type) {
           case '1': {
-            // 一周语文
-            axios.get(`/api/essay/${value.item_id}`)
-              .then((response) => {
-                store.commit(types.ESSAY, response.data.data);
-              }).catch((error) => {
-                store.commit(types.NET_STATUS, error);
-              });
-            break;
+            return {
+              request: axios.get(`/api/essay/${value.item_id}`), // 一周语文
+              type: types.ESSAY,
+            };
           }
           case '2': {
-            // 文章
-            axios.get(`/api/essay/${value.item_id}`)
-              .then((response) => {
-                store.commit(types.ESSAY, response.data.data);
-              }).catch((error) => {
-                store.commit(types.NET_STATUS, error);
-              });
-            break;
+            return {
+              request: axios.get(`/api/essay/${value.item_id}`), // 文章
+              type: types.ESSAY,
+            };
           }
           case '3': {
-            // 问答
-            axios.get(`/api/question/${value.item_id}`)
-              .then((response) => {
-                store.commit(types.QUESTION, response.data.data);
-              }).catch((error) => {
-                store.commit(types.NET_STATUS, error);
-              });
-            break;
+            return {
+              request: axios.get(`/api/question/${value.item_id}`), // 问答
+              type: types.QUESTION,
+            };
           }
           case '4': {
-            // 音乐
-            axios.get(`/api/music/detail/${value.item_id}`)
-              .then((response) => {
-                store.commit(types.MUSIC, response.data.data);
-              }).catch((error) => {
-                store.commit(types.NET_STATUS, error);
-              });
-            break;
+            return {
+              request: axios.get(`/api/music/detail/${value.item_id}`), // 音乐
+              type: types.MUSIC,
+            };
           }
           case '5': {
-            // 电影
-            axios.get(`api/movie/${value.item_id}/story/1/0`)
-              .then((response) => {
-                store.commit(types.MOVIE, response.data.data);
-              }).catch((error) => {
-                store.commit(types.NET_STATUS, error);
-              });
-            break;
+            return {
+              request: axios.get(`api/movie/${value.item_id}/story/1/0`), // 电影
+              type: types.MOVIE,
+            };
           }
           default: {
-            break;
+            return {
+              request: null,
+              type: null,
+            };
           }
         }
       });
+      await axios.all(oneListContent.map(value => value.request))
+        .then((response) => {
+          // 遍历oneListContent来对其进行分类commit
+          oneListContent.forEach((value, index) => {
+            // 确保不是空request
+            if (value.type) {
+              store.commit(value.type, response[index].data.data);
+            }
+          });
+        })
+        .catch((error) => {
+          store.commit(types.NET_STATUS, error);
+        });
     }
     next();
   },
